982e98128290a5f66451e0df9a902e4c68e03971
Barrytech <barryabdoul2024@gmail.com>
2020-06-07 03:52:11 -0400

    created a functionning program using objects

--- Practice7PersonObj/src/main/java/ClassDataApp.java
+++ Practice7PersonObj/src/main/java/ClassDataApp.java
@@ -10,7 +10,42 @@
  */
 public class ClassDataApp {
     public static void main (String[] args){
-        System.out.println("Hello world!");
+        int maxSize = 100;
+        ClassDataArray arr;
+        arr = new ClassDataArray(maxSize);
         
+        arr.insert("Evans", "West", 24);
+        arr.insert("Smith", "Lorraine", 19);
+        arr.insert("Yee", "Tom", 22);
+        arr.insert("Adams", "Grace", 19);
+        arr.insert("Murphy", "Grace", 20);
+        arr.insert("Barry", "Abdoul", 19);
+        arr.insert("Campo", "Ian", 28);
+        arr.insert("Cook", "Tim", 51);
+        arr.insert("Li", "Jet", 53);
+        arr.insert("Siluwa", "Timothy", 31);
+        
+        arr.displayarr();
+        
+        //lets search someone
+        String searchkey = "Murphy";
+        Person found;
+        found = arr.find(searchkey);
+        if(found != null){
+            System.out.println(" found ");
+            found.displayPerson();
+        }
+        else{
+            System.out.println(" Can't find " + searchkey);
+        }
+        
+        System.out.println(" Deleting Smith, Li, Yee");
+        arr.delete("Smith");
+        arr.delete("Li");
+        arr.delete("Yee");
+        
+        arr.displayarr();
+        
+         
     }
 }
--- Practice7PersonObj/src/main/java/Person.java
+++ Practice7PersonObj/src/main/java/Person.java
@@ -8,6 +8,104 @@
  *
  * @author barry
  */
+import java.io.*;
 public class Person {
+    private String lastName;
+    private String firstName;
+    private int age;
     
-}
+            //let's write the constructor method
+    public Person(String last, String first, int a){
+        lastName = last;
+        firstName = first;
+        age  = a;
+    }
+    
+    //let's write the display() method;
+    
+    public void displayPerson(){
+        System.out.println("Last Name is: " + lastName + ", First Name is: " + firstName + ", Age is: " + age);
+    }
+    
+    //let's write the getLast name method()
+    public String getLast(){
+        return lastName;
+    }
+    
+    
+       
+}       //closes Person class
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//let's write another class for the data
+class ClassDataArray{
+    private Person[] a;
+    private int nElems;
+    
+    //constructor
+    public ClassDataArray(int max){
+        a = new Person[max];
+        nElems = 0;
+    }
+    
+    //let's write the search and find() method
+    public Person find(String searchName){
+        int j;
+        for(j = 0; j < nElems; j++){
+            if(a[j].getLast().equals(searchName)){
+                break;
+            }
+        if( j == nElems){
+            return null;
+        }
+        else{
+            return a[j];
+        }
+        }
+        return null;
+    }       //end find()
+    
+    //let's write the insert method()
+    public void insert(String last, String first, int age){
+        a[nElems] = new Person(last, first, age);
+        nElems++;     
+    }       //end insert()
+    
+    //let's write the delete method()
+    public boolean delete(String searchName){
+        int i;
+        for(i = 0; i < nElems; i++){
+            if (a[i].getLast().equals(searchName)){
+                break;
+            }
+        if(i == nElems){
+            return false;
+        }
+        else{
+            for(int j=i; j<nElems; j++){
+                a[j] = a[j+1];
+                nElems--;
+                return true;
+            }
+        }
+        }
+        return false;       
+    }       //end delete method()   
+    
+    //let's write the display() method() after deleting
+    public void displayarr(){
+        System.out.println(" Here are the people objects in your array");
+        for(int i = 0; i<nElems; i++){
+            a[i].displayPerson();
+        }       
+        
+    }   //end display method()
+    
+}       //end classDataArray
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//let's write the 
